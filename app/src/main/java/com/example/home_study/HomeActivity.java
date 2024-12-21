c51a5af2c013bd6634ab212f1403d54b27ba735e
Bemnet <bemnettilahun700@gmail.com>
2024-12-21 18:56:54 +0300

    Home page is done

--- app/src/main/java/com/example/home_study/HomeActivity.java
+++ app/src/main/java/com/example/home_study/HomeActivity.java
@@ -1,14 +1,61 @@
 package com.example.home_study;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+    private ImageView chat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView =findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            final int ID = item.getItemId();
+
+            switch (ID){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+
+                case R.id.book:
+                    replaceFragment(new BookFragment());
+                    break;
+
+                case R.id.exam:
+                    replaceFragment(new ExamFragment());
+                    break;
+
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_layout, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
