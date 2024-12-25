c6dbcd7e387ea1b4f7a04b7f60289ad6a05297ab
Bemnet <bemnettilahun700@gmail.com>
2024-12-25 17:47:47 +0300

    Firestore to realtime changed

--- app/src/main/java/com/example/home_study/Adapter/PostAdapter.java
+++ app/src/main/java/com/example/home_study/Adapter/PostAdapter.java
@@ -13,17 +13,26 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.home_study.Model.Post;
 import com.example.home_study.Prevalent.Continuity;
 import com.example.home_study.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder>
 {
+
     private List<Post> postList;
 
     public PostAdapter(List<Post> postList) {
@@ -40,82 +49,133 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
     @Override
     public void onBindViewHolder(@NonNull PostAdapter.PostViewHolder holder, int position) {
         Post post = postList.get(position);
-
-
         String userId = Continuity.currentOnlineUser.getusername();
+        String postID = post.getPostId();
 
 
-        DocumentReference postRef = FirebaseFirestore.getInstance()
-                .collection("Posts")
-                .document(post.getPostId());
 
-        postRef.get().addOnSuccessListener(documentSnapshot -> {
-            if (documentSnapshot.exists()) {
-                List<String> likedBy = (List<String>) documentSnapshot.get("likedBy");
-                if (likedBy != null && likedBy.contains(userId)) {
+            holder.author.setText(post.getAuthor());
+            holder.postDate.setText(post.getTime());
+            holder.postMessage.setText(post.getMessage());
+            Picasso.get().load(post.getImageUrl()).placeholder(R.drawable.profile).into(holder.postImage);
+            Picasso.get().load(post.getAuthorProfile()).placeholder(R.drawable.profile).into(holder.authorProfile);
+            holder.likeCount.setText(post.getLikes()+"");
 
-                    holder.postLikeIcon.setImageResource(R.drawable.likefilled);
-                } else {
-                    holder.postLikeIcon.setImageResource(R.drawable.like);
-                }
-            }
-        });
+            DatabaseReference userLikesRef = FirebaseDatabase.getInstance().getReference()
+                    .child("Users")
+                    .child(userId)
+                    .child("liked");
 
-        holder.author.setText(post.getAuthor());
-        holder.postDate.setText(post.getTime());
-        holder.postMessage.setText(post.getMessage());
-        holder.postLikeIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String userId = Continuity.currentOnlineUser.getusername(); // Get the current user ID
+            DatabaseReference postLikesRef = FirebaseDatabase.getInstance().getReference()
+                    .child("Posts")
+                    .child(postID)
+                    .child("likes");
 
-                DocumentReference postRef = FirebaseFirestore.getInstance()
-                        .collection("Posts")
-                        .document(post.getPostId());
-
-                postRef.get().addOnSuccessListener(documentSnapshot -> {
-                    if (documentSnapshot.exists()) {
-                        List<String> likedBy = (List<String>) documentSnapshot.get("likedBy");
-                        if (likedBy == null) {
-
-                           likedBy = new ArrayList<>();
-                        }
-                        if (likedBy.contains(userId)){
-                            likedBy.remove(userId);
-                            postRef.update("likedBy", likedBy, "likes", likedBy.size());
-                            holder.postLikeIcon.setImageResource(R.drawable.like);
-                        }
-                        else {
-
-                            likedBy.add(userId);
-                            postRef.update("likedBy", likedBy, "likes", likedBy.size());
-                            holder.postLikeIcon.setImageResource(R.drawable.likefilled); // Liked state
-                        }
-
-                        holder.likeCount.setText(String.valueOf(likedBy.size()));
+//        check if the user like the post
+            userLikesRef.child(postID).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists())
+                    {
+                        holder.postLikeIcon.setImageResource(R.drawable.likefilled);
                     }
-                });
-            }
-        });
-        holder.postComment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(v.getContext(), "Comment Unavailable", Toast.LENGTH_SHORT).show();
-            }
-        });
-        Picasso.get().load(post.getImageUrl()).placeholder(R.drawable.profile).into(holder.postImage);
-        Picasso.get().load(post.getAuthorProfile()).placeholder(R.drawable.profile).into(holder.authorProfile);
-        holder.likeCount.setText(post.getLikes()+"");
-    }
+                    else {
+                        holder.postLikeIcon.setImageResource(R.drawable.like);
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+
+
+
+            holder.postLikeIcon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    userLikesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            if (snapshot.exists()) {
+
+
+                                userLikesRef.child(postID).removeValue();
+                                postLikesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        if (snapshot.exists()) {
+                                            int currentLikes = snapshot.getValue(Integer.class);
+                                            postLikesRef.setValue(currentLikes - 1);
+                                            holder.likeCount.setText(String.valueOf(currentLikes - 1));
+                                            holder.postLikeIcon.setImageResource(R.drawable.like);
+                                        } else {
+
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+                            }else {
+
+                                userLikesRef.child(postID).setValue(true);
+                                postLikesRef.child(postID).addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                        if (snapshot.exists()) {
+                                            int currentLikes = snapshot.getValue(Integer.class);
+                                            postLikesRef.setValue(currentLikes + 1);
+                                            holder.likeCount.setText(String.valueOf(currentLikes + 1));
+                                            holder.postLikeIcon.setImageResource(R.drawable.likefilled);
+                                        } else {
+                                            postLikesRef.setValue(1);
+                                            holder.likeCount.setText("1");
+                                            holder.postLikeIcon.setImageResource(R.drawable.likefilled);
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError error) {
+
+                                    }
+                                });
+                            }
+                        }
+                        @Override
+                        public void onCancelled (@NonNull DatabaseError error){
+
+                        }
+
+                    });
+
+                }
+            });
+            holder.postComment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(v.getContext(), "Comment Unavailable", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+
+
+
 
     @Override
     public int getItemCount() {
-        return postList.size();
+        return postList != null ? postList.size():0;
     }
 
     public class PostViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView author, postDate, postMessage, likeCount;
+        private TextView author, postDate, postMessage, likeCount, noPost;
 
         private ImageView postImage, postLikeIcon, postComment;
 
@@ -123,6 +183,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder
         public PostViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            noPost = itemView.findViewById(R.id.no_post);
             authorProfile = itemView.findViewById(R.id.author_profile);
             author =  itemView.findViewById(R.id.postAuthor);
             postDate = itemView.findViewById(R.id.postTime);
