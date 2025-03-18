91afddceb85e99d5b893040d461109a6882fc681
Bemnet <bemnettilahun700@gmail.com>
2025-03-18 19:26:33 +0300

    Exam fragement Skeleton is complete

--- app/src/main/java/com/example/home_study/ExamCenterActivity.java
+++ app/src/main/java/com/example/home_study/ExamCenterActivity.java
@@ -2,13 +2,16 @@ package com.example.home_study;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.home_study.Adapter.ExamCenterAdapter;
 import com.example.home_study.Model.ExamCenter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,8 +24,11 @@ import java.util.List;
 
 public class ExamCenterActivity extends AppCompatActivity {
 
-    private RecyclerView questionRecycler;
-    private ExamCenterAdapter examCenterAdapter;
+    private TextView question, explanation, questionCounter, title;
+    private Button nextBtn, previousBtn;
+    private ImageView backBtn;
+    private RadioGroup chooses;
+    private int currentQuestionIndex = 0;
     private List<ExamCenter> questionList;
     private DatabaseReference questionRef;
     private String selectedSubject, selectedChapter;
@@ -31,22 +37,87 @@ public class ExamCenterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam_center);
-        
-        questionRecycler = findViewById(R.id.examCenterRecycler);
-        questionRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        nextBtn = findViewById(R.id.nextBtn);
+        previousBtn = findViewById(R.id.previousBtn);
+        backBtn = findViewById(R.id.backBtn);
+        questionCounter = findViewById(R.id.questionCounter);
+        title = findViewById(R.id.title);
+        question = findViewById(R.id.question);
+        explanation = findViewById(R.id.explanation);
+
+        chooses = findViewById(R.id.questionChoose);
+
         
         selectedSubject = getIntent().getStringExtra("subject");
         selectedChapter = getIntent().getStringExtra("chapter");
         
         questionList = new ArrayList<>();
-        examCenterAdapter = new ExamCenterAdapter(questionList);
-        
-        questionRecycler.setAdapter(examCenterAdapter);
 
+        title.setText(selectedChapter);
         questionRef = FirebaseDatabase.getInstance().getReference().child("Exams")
                         .child(selectedSubject).child(selectedChapter);
-        
+
         fetchQuestions();
+
+        backBtn.setOnClickListener(v ->{
+            finish();
+        });
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentQuestionIndex < questionList.size() - 1){
+                    currentQuestionIndex++;
+                    displayQuestion();
+                }
+            }
+        });
+
+        previousBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentQuestionIndex > 0){
+                    currentQuestionIndex--;
+                    displayQuestion();
+                }
+            }
+        });
+
+
+    }
+
+    private void displayQuestion() {
+
+        if (questionList.isEmpty()) return;
+
+        ExamCenter questions = questionList.get(currentQuestionIndex);
+        question.setText((currentQuestionIndex + 1) + ". " +questions.getQuestion());
+        questionCounter.setText("Question " + (currentQuestionIndex + 1) + " of " + questionList.size());
+
+        chooses.removeAllViews();
+        for (String option : questions.getOptions()){
+            RadioButton radioButton = new RadioButton(this);
+            radioButton.setText(option);
+            chooses.addView(radioButton);
+        }
+
+        chooses.setOnCheckedChangeListener((group, checkedId) ->{
+            RadioButton selectedRadioButton = group.findViewById(checkedId);
+
+            if (selectedRadioButton != null){
+                String selectedAnswer = selectedRadioButton.getText().toString();
+
+                if (selectedAnswer.equals(questions.getCorrectAnswer()))
+                {
+                   explanation.setText("✅ Correct! " + questions.getExplanation());
+//                            holder.explanation.setTextColor(R.color.Green);
+                }else {
+                    explanation.setText("❌ Incorrect! " + questions.getExplanation());
+                }
+            }
+        });
+
+
     }
 
     private void fetchQuestions() {
@@ -60,9 +131,17 @@ public class ExamCenterActivity extends AppCompatActivity {
                     questionList.add(question);
                 }
 
-                examCenterAdapter.notifyDataSetChanged();
+                if (!questionList.isEmpty()){
+                    displayQuestion();
+                }else {
+                    question.setText("No question available");
+                    chooses.removeAllViews();
+                    explanation.setText("");
+                    questionCounter.setText("Question" + 0 + " of " + 0);
+                    previousBtn.setEnabled(false);
+                    nextBtn.setEnabled(false);
+                }
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(ExamCenterActivity.this, "Failed to loaded questions", Toast.LENGTH_SHORT).show();
